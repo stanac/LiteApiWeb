3981d7c4b2632f005635c2f6aae1dca594f3a277
Milan Stanacev <milan@stanacev.com>
2017-05-03 17:08:09 +0200

    add port

--- LiteApiWeb/Program.cs
+++ LiteApiWeb/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using System.Diagnostics;
 
 namespace LiteApiWeb
 {
@@ -11,14 +12,38 @@ namespace LiteApiWeb
     {
         public static void Main(string[] args)
         {
+            bool isDebugging = Debugger.IsAttached;
+
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+                .UseContentRoot(Directory.GetCurrentDirectory());
 
-            host.Run();
+            if (isDebugging) host.UseIISIntegration();
+            else host.UseUrls($"http://localhost:{GetPort()}");
+
+            host.UseStartup<Startup>();
+
+            host.Build().Run();
+        }
+
+        private static int GetPort()
+        {
+            int port = 4772;
+            try
+            {
+                string dir = Directory.GetCurrentDirectory();
+                var file = Directory.GetFiles(dir, "port").FirstOrDefault();
+                if (file != null)
+                {
+                    string text = File.ReadAllText(file);
+                    if (int.TryParse(text, out port)) return port;
+                }
+            }
+            catch
+            {
+                return 4772;
+            }
+            return 4772;
         }
     }
 }
