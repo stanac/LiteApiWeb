a85b12826284f5fa4091b1ad3468e5417edde805
Milan Stanacev <milan@stanacev.com>
2017-05-04 08:38:29 +0200

    remove content reference in project file

--- LiteApiWeb/Startup.cs
+++ LiteApiWeb/Startup.cs
@@ -51,7 +51,7 @@ namespace LiteApiWeb
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            // loggerFactory.AddConsole();
 
             if (_enableFirstRunRender)
             {
